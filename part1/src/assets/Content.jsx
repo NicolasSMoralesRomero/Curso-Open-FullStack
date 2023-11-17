9ac199ba0c645156a5561cb7e7a75b8123d727e4
Nicolas <nicosmr56@gmail.com>
2023-11-17 13:22:08 -0300

    prueba commit

---
+++ part1/src/assets/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "./Part";
+
+const Content = ({part1, part2, part3}) => {
+    return(
+      <div>
+        <Part part1={part1} />
+        <Part part2={part2} />
+        <Part part3={part3} />
+      </div>
+    )
+  }
+
+  export default Content;
